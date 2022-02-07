cbe584abfa46bd4b5d2b09dfed5bb1387de7e849
JoshyPHP <s9e.dev@gmail.com>
2022-02-07 17:29:45 +0100

    Replaced test namespace

--- tests/BuildTest.php
+++ tests/BuildTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace s9e\RegexpBuilder\Tests;
+namespace s9e\RegexpBuilder\Command\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
